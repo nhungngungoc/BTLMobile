f1e3f4c854c0df495ff56ffb60c325f31a45e7a8
nhungngungoc <nhungthiem20102002@gmail.com>
2023-10-09 20:10:13 +0700

    nhung

--- app/src/main/java/com/example/btl_laptrinhmobile/MainActivity.java
+++ app/src/main/java/com/example/btl_laptrinhmobile/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.ActionBarPolicy;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,10 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +28,11 @@ public class MainActivity extends AppCompatActivity
     private int curentDay = 0;
 
     private int index = 0;
+
+    private List<String> calendarStrings;
+    int[] days;
+    int[] months;
+    int[] years;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +42,9 @@ public class MainActivity extends AppCompatActivity
 
         final List<String> calendarStrings = new ArrayList<>();
         final int numberOfDays = 2000;
-        final int[] days = new int[numberOfDays];
-        final int[] months = new int[numberOfDays];
-        final int[] years = new int[numberOfDays];
+        days = new int[numberOfDays];
+        months = new int[numberOfDays];
+        years = new int[numberOfDays];
 
         final EditText textInput = findViewById(R.id.textInput);
 
@@ -51,18 +61,21 @@ public class MainActivity extends AppCompatActivity
                     dayContent.setVisibility(View.VISIBLE);
                 }
 
-
-
-                for (int i = 0; i<30; i++){
-                    if (days[i]==curentDay) {
-                        for (int j =0;i<12; i++){
-                            if (months[j]==curentMonth){
-                                textInput.setText(calendarStrings.get(i));
-                                return;
+                for (int h=0; h<index; h++){
+                    if (years[h]==curentYear){
+                        for (int i = 0; i<index; i++){
+                            if (days[i]==curentDay) {
+                                for (int j =0;i<index; i++){
+                                    if (months[j]==curentMonth && days[j]==curentDay && years[j]==curentYear){
+                                        textInput.setText(calendarStrings.get(j));
+                                        return;
+                                    }
+                                }
                             }
                         }
                     }
                 }
+
                 textInput.setText("");
             }
         });
@@ -75,10 +88,37 @@ public class MainActivity extends AppCompatActivity
                 days[index] = curentDay;
                 months[index] = curentMonth;
                 years[index] = curentYear;
-                index++;
                 calendarStrings.add(index,textInput.getText().toString());
                 textInput.setText("");
+                index++;
+                dayContent.setVisibility(View.GONE);
             }
         });
+
+        final Button todayButton = findViewById(R.id.todayButton);
+        todayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              calendarView.setDate(calendarView.getDate());
+            }
+        });
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        saveInfo();
+    }
+    private void saveInfo(){
+        File file = new File(this.getFilesDir(), "calendarStrings");
+        try {
+            FileWriter fileWriter = new FileWriter("calendarStrings");
+            int calendarStringsCount= calendarStrings.size();
+            for (int i=0; i<calendarStringsCount; i++){
+                fileWriter.write(calendarStrings.get(i));
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
