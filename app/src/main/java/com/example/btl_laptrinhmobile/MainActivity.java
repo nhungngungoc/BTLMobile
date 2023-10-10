aa2246c8e5bd7a2b431a8759c87f6ff826d3ca5d
nhungngungoc <nhungthiem20102002@gmail.com>
2023-10-10 10:10:20 +0700

    nhung

--- app/src/main/java/com/example/btl_laptrinhmobile/MainActivity.java
+++ app/src/main/java/com/example/btl_laptrinhmobile/MainActivity.java
@@ -13,10 +13,16 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilterReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity
     int[] days;
     int[] months;
     int[] years;
+    int numberOfDays = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +52,8 @@ public class MainActivity extends AppCompatActivity
         days = new int[numberOfDays];
         months = new int[numberOfDays];
         years = new int[numberOfDays];
+
+        readInfo();
 
         final EditText textInput = findViewById(R.id.textInput);
 
@@ -103,22 +112,65 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
+
+
+
     @Override
     protected void onPause(){
         super.onPause();
         saveInfo();
     }
     private void saveInfo(){
-        File file = new File(this.getFilesDir(), "calendarStrings");
+        File file = new File(this.getFilesDir(),"saved");
+        File daysFile = new File(this.getFilesDir(),"days");
+        File monthsFile = new File(this.getFilesDir(),"months");
+        File yearsFile = new File(this.getFilesDir(), "years");
+
         try {
-            FileWriter fileWriter = new FileWriter("calendarStrings");
-            int calendarStringsCount= calendarStrings.size();
-            for (int i=0; i<calendarStringsCount; i++){
-                fileWriter.write(calendarStrings.get(i));
+            FileOutputStream fOut = new FileOutputStream(file);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fOut));
+
+            FileOutputStream fOutDays = new FileOutputStream(daysFile);
+            BufferedWriter bwDay = new BufferedWriter(new OutputStreamWriter(fOutDays));
+
+            FileOutputStream fOutMonths = new FileOutputStream(monthsFile);
+            BufferedWriter bwMonths = new BufferedWriter(new OutputStreamWriter(fOutMonths));
+
+            FileOutputStream fOutYears = new FileOutputStream(yearsFile);
+            BufferedWriter bwYears = new BufferedWriter(new OutputStreamWriter(fOutYears));
+
+            for (int i  = 0; i<index; i++){
+                bw.write(calendarStrings.get(i));
+                bw.newLine();
+                bwDay.write(days[i]);
+                bwMonths.write(months[i]);
+                bwYears.write(years[i]);
             }
-            fileWriter.close();
-        } catch (IOException e) {
+
+            bw.close();
+            fOut.close();
+            bwDay.close();
+            fOutDays.close();
+            bwMonths.close();
+            fOutMonths.close();
+            bwYears.close();
+            fOutYears.close();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
+    private void readInfo(){
+        File file = new File(this.getFilesDir(),"saved");
+        File daysFile = new File(this.getFilesDir(),"days");
+        File monthsFile = new File(this.getFilesDir(),"months");
+        File yearsFile = new File(this.getFilesDir(), "years");
+
+        if (file.exists()){
+            return;
+        }
+
+    }
+
 }
